2a46674adf3af46c13521bf9d2da89176a3740cd
hyndaviredd <130479488+hyndaviredd@users.noreply.github.com>
2023-05-14 21:45:34 +0530

    Add files via upload

--- page replcement.c
+++ page replcement.c
@@ -1,47 +1,121 @@
-#include<stdio.h>  
-int main()  
-{  
-    int incomingStream[] = {4 , 1 , 2 , 4 , 3 , 2 , 1 , 5}; 
-    int pageFaults = 0;  
-    int frames = 3;  
-    int m, n, s, pages;   
-    pages = sizeof(incomingStream)/sizeof(incomingStream[0]);   
-    	printf(" Incoming \ t Frame 1 \ t  2 \ t Frame 3 ");  
-    int temp[ frames ];  
-    for(m = 0; m < frames; m++)  
-    {  
-        temp[m] = -1;  
-    }  
-    for(m = 0; m < pages; m++)  
-    {  
-        s = 0;   
-        for(n = 0; n < frames; n++)  
-        {  
-            if(incomingStream[m] == temp[n])  
-            {  
-                s++;  
-                pageFaults--;  
-            }  
-        }  
-        pageFaults++;  
-        if((pageFaults <= frames) && (s == 0))  
-        {  
-            temp[m] = incomingStream[m];  
-        }  
-        else if(s == 0)  
-        {  
-            temp[(pageFaults - 1) % frames] = incomingStream[m];  
-        }  
-        printf("\n");  
-        printf("%d\t\t\t",incomingStream[m]);  
-        for(n = 0; n < frames; n++)  
-        {  
-            if(temp[n] != -1)  
-                printf(" %d\t\t\t", temp[n]);  
-            else  
-                printf(" - \t\t\t");  
-        }  
-    }  
-    printf("\nTotal Page Faults:\t%d\n", pageFaults);  
-    return 0;  
+#include <bits/stdc++.h>
+using namespace std;
+ 
+struct Process {
+    int pid; // Process ID
+    int bt; // Burst Time
+    int art; // Arrival Time
+};
+ void findWaitingTime(Process proc[], int n,
+                                int wt[])
+{
+    int rt[n];
+ 
+    
+    for (int i = 0; i < n; i++)
+        rt[i] = proc[i].bt;
+ 
+    int complete = 0, t = 0, minm = INT_MAX;
+    int shortest = 0, finish_time;
+    bool check = false;
+ 
+    
+    while (complete != n) {
+ 
+        
+        for (int j = 0; j < n; j++) {
+            if ((proc[j].art <= t) &&
+            (rt[j] < minm) && rt[j] > 0) {
+                minm = rt[j];
+                shortest = j;
+                check = true;
+            }
+        }
+ 
+        if (check == false) {
+            t++;
+            continue;
+        }
+ 
+       
+        rt[shortest]--;
+ 
+        
+        minm = rt[shortest];
+        if (minm == 0)
+            minm = INT_MAX;
+ 
+        
+        if (rt[shortest] == 0) {
+ 
+           
+            complete++;
+            check = false;
+ 
+            
+            finish_time = t + 1;
+ 
+            
+            wt[shortest] = finish_time -
+                        proc[shortest].bt -
+                        proc[shortest].art;
+ 
+            if (wt[shortest] < 0)
+                wt[shortest] = 0;
+        }
+       
+        t++;
+    }
+}
+ 
+
+void findTurnAroundTime(Process proc[], int n,
+                        int wt[], int tat[])
+{
+    
+    for (int i = 0; i < n; i++)
+        tat[i] = proc[i].bt + wt[i];
+}
+ 
+
+void findavgTime(Process proc[], int n)
+{
+    int wt[n], tat[n], total_wt = 0,
+                    total_tat = 0;
+ 
+
+    findWaitingTime(proc, n, wt);
+ 
+    
+    findTurnAroundTime(proc, n, wt, tat);
+ 
+    
+    cout << " P\t\t"
+        << "BT\t\t"
+        << "WT\t\t"
+        << "TAT\t\t\n";
+ 
+    for (int i = 0; i < n; i++) {
+        total_wt = total_wt + wt[i];
+        total_tat = total_tat + tat[i];
+        cout << " " << proc[i].pid << "\t\t"
+            << proc[i].bt << "\t\t " << wt[i]
+            << "\t\t " << tat[i] << endl;
+    }
+ 
+    cout << "\nAverage waiting time = "
+        << (float)total_wt / (float)n;
+    cout << "\nAverage turn around time = "
+        << (float)total_tat / (float)n;
+}
+ 
+
+int main()
+{
+    Process proc[] = { { 1, 6, 2 }, { 2, 2, 5 },
+                    { 3, 8, 1 }, { 4, 3, 0}, {5, 4, 4} };
+    int n = sizeof(proc) / sizeof(proc[0]);
+ 
+    findavgTime(proc, n);
+    return 0;
 }
